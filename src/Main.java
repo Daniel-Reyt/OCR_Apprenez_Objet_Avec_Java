57eb599ea7608d8ecb0431c32293aa3df2998406
daniel.reyt <daniel-david.reyt@uha.fr>
2020-09-16 15:56:34 +0200

    pt5 of course

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
-public class Main {
+import garage.Car;
 
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Clio", "renault clio 3", "renault", 2005, "gris mat", 225, new int[] {4832,1720,1497},
+        1240, 4, 150, "diesel", List.of(new String[]{"vitres électriques", "gps"}));
+    }
 }
